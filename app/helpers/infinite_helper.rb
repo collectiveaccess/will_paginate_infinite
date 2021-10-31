95cbe26e12b0c156e24e27cb794a6689c1a242a2
Seth <seth@collectiveaccess.org>
2021-10-31 00:27:54 -0400

    Add  multi-tab support for the Exchange

--- app/helpers/infinite_helper.rb
+++ app/helpers/infinite_helper.rb
@@ -6,10 +6,10 @@ module InfiniteHelper
     html = "$('" + containerSelector + "').append('"+ j(render render_options) + "');"
 
     if collection.next_page
-      html += "$('.infinite-pagination').replaceWith('" + j(will_paginate(collection, renderer: WillPaginateInfinite::InfinitePagination)) + "');"
+      html += "$('" + containerSelector + "').parent().find('.infinite-pagination').replaceWith('" + j(will_paginate(collection, renderer: WillPaginateInfinite::InfinitePagination)) + "');"
     else
-      html += "$(window).off('scroll');"
-      html += "$('.infinite-pagination').remove();"
+      html += "$('" + containerSelector + "').off('scroll');"
+      html += "$('" + containerSelector + "').parent().find('.infinite-pagination').remove();"
     end
 
     html.html_safe
